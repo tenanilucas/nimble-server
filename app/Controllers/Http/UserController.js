a8a91f8bc9b11a7121f55d9a656a7baaf5d3bd12
Lucas Tenani <lucas.tenani@espacolaser.com.br>
2021-08-26 20:06:37 -0300

    configurando cadastro e login de usuários

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,17 @@
-'use strict'
+"use strict"
+
+const User = use("App/Models/User")
 
 class UserController {
+  async create({
+    request
+  }) {
+    const data = request.only(["username", "email", "password"])
+
+    const user = await User.create(data)
+
+    return user
+  }
 }
 
 module.exports = UserController
